103a8668e005406e573a49cd5326ad4ea71f9d0a
Christian Kruse <christian@c-kruse.com>
2024-10-08 16:42:34 -0700

    add plumbing for local registry
    Signed-off-by: Christian Kruse <christian@c-kruse.com>

--- tests/contour/install.sh
+++ tests/contour/install.sh
@@ -1,14 +1,50 @@
 #!/usr/bin/env bash
 
-kind create cluster -n contour --wait 60s
+reg_name='kind-registry'
+reg_port='5000'
+cat <<EOF | kind create cluster -n contour --wait 60s --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry]
+    config_path = "/etc/containerd/certs.d"
+EOF
+
+REGISTRY_DIR="/etc/containerd/certs.d/localhost:${reg_port}"
+for node in $(kind get nodes); do
+  docker exec "${node}" mkdir -p "${REGISTRY_DIR}"
+  cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${REGISTRY_DIR}/hosts.toml"
+[host."http://${reg_name}:5000"]
+EOF
+done
+
+# 4. Connect the registry to the cluster network if not already connected
+# This allows kind to bootstrap the network but ensures they're on the same network
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
+  docker network connect "kind" "${reg_name}"
+fi
+
+# 5. Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
 
 helm repo add metallb https://metallb.github.io/metallb
 
 helm upgrade --install metallb metallb/metallb -n metallb-system --create-namespace -f metallb-values.yaml --wait
 
-cluster=1
 kind_ip=$(docker network inspect -f '{{.IPAM.Config}}' kind | awk '/.*/ { print $2 }')
-start_ip=$(echo "$kind_ip" | cut -f1-2 -d'.')."${cluster_number[$cluster]}".100
+start_ip=$(echo "$kind_ip" | cut -f1-2 -d'.').200.100
 end_ip=$(echo "$start_ip" | cut -f1-3 -d'.').250
 
 kubectl apply -f - <<EOF
